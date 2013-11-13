504acf59468d2d20788f9ddd4ebabcb5fd83df3f
Kristi Hupka <hupka@usc.edu>
2013-11-12 19:30:16 -0800

    Adding updates for person and role

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,11 +1,42 @@
 package person;
 
+import java.util.*;
+
 import agent.Agent;
 
 public class Person extends Agent {
+	
+	//Data
+	List<Role> roles; 	//contains all the customer roles
+	double money;
+	HashMap <String, Integer> Inventory; 		//market list
+	boolean hasCar;
+	int accountNum;
+	double accountBalance;
+	boolean hasFoodInFridge;
+	int sleepTime = 22;
 
-	protected boolean pickAndExecuteAnAction() {
-		return false;
+	//Messages
+	void msgNewTime(int time) {
 	}
 
+	//Scheduler
+	protected boolean pickAndExecuteAnAction() {
+//		if (exists a role in role such that r. is active)
+//			then r.pickAndExecuteAnAction();
+//		if (exists a role such that r. is watingToExecute) {
+//			then SetRoleActive(role);	
+//			return true;
+//		}
+//		if (no role is active) {
+//			GoHome();
+//			return false;
+//		}
+	}
+
+	//Actions
+	SetRoleActive(role) {
+		role.state = active;
+		stateChanged();	
+	}
 }
