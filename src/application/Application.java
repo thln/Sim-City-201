22c324ebdc08c6de01990a208a16a1ff66d3c517
Carmen Tan <carmenta@usc.edu>
2013-11-13 02:20:42 -0800

    Major layout for the different panels

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,14 +3,40 @@ package application;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.util.List;
 
 public class Application extends JFrame implements ActionListener{
-
+	
+	CityPanel cityPanel = new CityPanel();
+	
+	//List containing all of the different building animation panels
+	public List<CityPanel> cityPanels = new ArrayList<CityPanel>();
+	CityPanel cityPanel1 = new CityPanel();
+	CityPanel cityPanel2 = new CityPanel();
+	CityPanel cityPanel3 = new CityPanel();
+	ControlPanel controlPanel = new ControlPanel();
+	
 	public Application() {
 		int WINDOWX = 1200;
         int WINDOWY = 800;
        	setBounds(50, 50, WINDOWX, WINDOWY);
     	setLayout(null);
+    	
+    	//here we have the main city view
+    	cityPanel.setBounds(WINDOWX/2, 0, WINDOWX/2, WINDOWY/2);
+    	
+    	//stacking the building animations
+    	cityPanel1.setBounds(WINDOWX/2, WINDOWY/2, WINDOWX/2, WINDOWY/2);
+    	cityPanel2.setBounds(WINDOWX/2+50, WINDOWY/2, WINDOWX/2, WINDOWY/2);
+    	cityPanel3.setBounds(WINDOWX/2+100, WINDOWY/2, WINDOWX/2, WINDOWY/2);
+    	
+    	add(controlPanel);
+    	add(cityPanel);
+    	
+    	add(cityPanel1);
+    	add(cityPanel2);
+    	add(cityPanel3);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
--- src/application/CityPanel.java
+++ src/application/CityPanel.java
@@ -3,15 +3,47 @@ package application;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.imageio.*;
+
+import java.awt.image.BufferedImage;
 
 public class CityPanel extends JPanel implements ActionListener{
 	
+    private final int WINDOWX = 450;
+    private final int WINDOWY = 400;
+    
+    //list of images representing our different buildings
+    BufferedImage carIcon = null;
+    BufferedImage bankIcon = null;
+    BufferedImage marketIcon = null;
+    BufferedImage houseIcon = null;
+    BufferedImage restaurantIcon = null;
+	
 	public CityPanel() {
-		
+	   	setSize(WINDOWX, WINDOWY);
+        setVisible(true);
+        setLayout(null);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		repaint();
 	}
+	
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        //Clear the screen by painting a rectangle the size of the frame
+        g2.setColor(getBackground());
+        g2.fillRect(0, 0, WINDOWX, WINDOWY );
+
+        //Here is the table
+        g2.setColor(Color.ORANGE);
+
+        for(int ix=1; ix<= 5; ix++) {
+        	g2.fillRect(50*ix, 50, 40, 40);
+        }
+        
+    }
 
 }
