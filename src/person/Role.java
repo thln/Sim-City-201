73cfb31cfb5d228d2a1b3dc91915c0267feca173
Kristi Hupka <hupka@usc.edu>
2013-11-12 20:17:38 -0800

    adding methods to role class

--- src/person/Role.java
+++ src/person/Role.java
@@ -1,7 +1,25 @@
 package person;
 
-public class Role {
+public abstract class Role {
 
+	Person person;
+	
 	enum roleState {active, inActive, waitingToExecute}
 	roleState state = roleState.inActive;
+	
+	Role(Person person) {
+		this.person = person;
+	}
+	
+	protected void stateChanged() {
+        //calls Person scheduler
+		person.pickAndExecuteAnAction();
+    }
+
+    protected abstract boolean pickAndExecuteAnAction();
+    
+    protected void inactivateRole() {
+    	state = roleState.inActive;
+    	stateChanged();
+    }
 }
