a9a24e031391cfeb8fa9ba1255c81746f3cb1283
Kristi Hupka <hupka@usc.edu>
2013-11-14 14:04:02 -0800

    Creating Market Class

--- src/market/Market.java
+++ src/market/Market.java
@@ -1,12 +1,23 @@
 package market;
 
+import person.Person;
+
 public class Market {
 
 	double money;
 	
 	SalesPerson salesPersonRole;
+	UPSman	UPSmanRole;
 	
-	public void msgIWantProducts(MarketCustomer customer, String item, int numWanted) {
+	public void iWantProducts(MarketCustomer customer, String item, int numWanted) {
 		salesPersonRole.msgIWantProducts(customer, item, numWanted);
+	}
+	
+	public SalesPerson arrivedAtWorkSalesPerson(Person person) {
+		return salesPersonRole;
+	}
+	
+	public UPSman arrivedAtWorkUPSman(Person person) {
+		return UPSmanRole;
 	}
 }
