c8b05a9d941cb2eb35258020f4609e221e5537dd
Carmen Tan <carmenta@usc.edu>
2013-11-14 01:59:33 -0800

    stacking works. showing panels at mouseclick

--- src/application/gui/AnimationPanel.java
+++ src/application/gui/AnimationPanel.java
@@ -5,21 +5,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
+import java.io.*;
 
-public class AnimationPanel extends JPanel implements ActionListener {
-	
+import javax.imageio.*;
+
+import java.awt.image.BufferedImage;
+
+public class AnimationPanel extends JPanel implements MouseListener {
 	
 	CityPanel cityPanel = new CityPanel();
 	public List<JPanel> buildingPanels = new ArrayList<JPanel>();
-	//JPanel buildingPanel = new JFrame("Building View");
+	public List<JLabel> imglabels = new ArrayList<JLabel>();
+	public Map<JLabel, JPanel> buildingMap = new HashMap<JLabel, JPanel>();
 	
-	final static int CityDimX = 600; //City View Frame Width
-	final static int CityDimY = 300; //City View Frame Height
-	final static int BuildingDimX = 600; //Building View Frame Width
-	final static int BuildingDimY = 300; //Building View Frame Height
-	
+	BufferedImage bankicon = null;
 	
 	AnimationPanel(){
 		
@@ -28,41 +31,80 @@ public class AnimationPanel extends JPanel implements ActionListener {
 		setBorder(BorderFactory.createTitledBorder("Animation"));
     	//here we have the main city view
     	
-		//cityPanel.setSize(700, 400);
 		cityPanel.setBounds(3, 15, 700, 400); //x & y positions in animation panel, x & y sizes
 		/*
     	cityPanel.setPreferredSize(cityDim);
     	cityPanel.setMinimumSize(cityDim);
     	cityPanel.setMaximumSize(cityDim);
     	*/
+		cityPanel.addMouseListener(this);
     	cityPanel.setVisible(true);
     	add(cityPanel);
     	
     	
     	//stacking the building animations
-    	//add(buildingPanel1)
-    	//add(buiildingPanel2)
+    	ImageIcon icon = new ImageIcon("chef.png",
+                "a pretty but meaningless splat");
+
+    	//initializing some stubs to test the stacking
+    	for(int i=0; i < 5; i++) {
+    		CityPanel building = new CityPanel();
+    		buildingPanels.add(building);
+    		JLabel label = new JLabel("Label " + (i+1), icon, JLabel.TRAILING);
+    		//imglabels.add(label);
+    		imglabels.add(label);
+    		buildingMap.put(label, building);
+    	}
     	
-    	
-    	
-    	
-		
-		
-		
-		
-		
+    	for(int n=0; n<buildingPanels.size();n++) {
+    		Dimension size = imglabels.get(n).getPreferredSize();
+    		imglabels.get(n).setBounds(size.width*n, 0, size.width, size.height);
+    		imglabels.get(n).addMouseListener(this);
+    		cityPanel.add(imglabels.get(n));
+    		buildingPanels.get(n).setBounds(3 + 50*n, 415, 700, 400);
+    		
+    		buildingPanels.get(n).setVisible(false);
+    		
+    		add(buildingPanels.get(n));
+    	}
 	}
 
-
+	public void clearScreen() {
+		for(int n=0; n<buildingPanels.size();n++) {
+			buildingPanels.get(n).setVisible(false);
+		}
+	}
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
-		
+		//If any of the icons are clicked, it will find and display its corresponding animation panel
+		for(int i=0; i<imglabels.size();i++) {
+			if(e.getSource() == imglabels.get(i)) {
+				final String name = imglabels.get(i).getText();
+				System.out.println(name + " clicked " + buildingMap.get(imglabels.get(i)));
+				clearScreen();
+				buildingMap.get(imglabels.get(i)).setVisible(true);
+			}
+		}
+		if(e.getSource() == cityPanel) {
+			System.out.println("Panel Clicked!!");
+		}
+	}
+	
+	public void mousePressed(MouseEvent e) {
 		
 	}
 	
+	public void mouseReleased(MouseEvent e) {
+		
+	}
 	
+	public void mouseEntered(MouseEvent e) {
+		
+	}
 	
-	
+	public void mouseExited(MouseEvent e) {
+		
+	}
 }
