38946809915ad89c6b6cc9fab30361869880fd1c
Kristi Hupka <hupka@usc.edu>
2013-11-14 18:39:13 -0800

    Updated UPSman

--- src/market/UPSman.java
+++ src/market/UPSman.java
@@ -1,13 +1,38 @@
 package market;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import person.Role;
+import restaurant.Order;
 
 public class UPSman extends Role {
 
-	@Override
+	//Data
+	private List<Order> orders = Collections.synchronizedList(new ArrayList<Order>());
+
+	//Messages
+	public void msgDeliverOrder(Order o) {
+		orders.add(o);
+		stateChanged();
+	}
+	
+	//Scheduler
 	protected boolean pickAndExecuteAnAction() {
-		// TODO Auto-generated method stub
+		if (!orders.isEmpty()) {
+			for (Order o: orders) {
+				deliverOrder(o);
+				return true;
+			}
+		}
 		return false;
+	}
+	
+	//Actions
+	public void deliverOrder(Order o) {
+		o.customer.HereIsOrder(o);
+		orders.remove(o);
 	}
 
 }
