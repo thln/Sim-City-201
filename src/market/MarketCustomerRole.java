ec5c76ca6193906b337fc235de377bd088e225e0
Kristi Hupka <hupka@usc.edu>
2013-11-16 11:51:26 -0800

    Re-did the MarketCustomerRole

--- src/market/MarketCustomerRole.java
+++ src/market/MarketCustomerRole.java
@@ -4,30 +4,54 @@ import person.Person;
 import person.Role;
 
 public class MarketCustomerRole extends Role {
-
-//	enum CustomerDesire {none, buyCar, buyFood};
-	
 	protected String RoleName = "Market Customer";
-	CustomerDesire desire = CustomerDesire.none;
-	
+
+	//Data
+	boolean recievedItems = false;
+	double money;
+	double bill;
+	String item;
+
+	//Agent correspondents
+	SalesPersonRole salesPerson;
+
+	//	enum CustomerDesire {none, buyCar, buyFood};
+	//	CustomerDesire desire = CustomerDesire.none;
+
 	public MarketCustomerRole(Person person) {
 		super(person);
 	}
 
-	public void msgHereAreYourThings(String item, double orderCost) {
-		
+	//Messages
+	public void msgHereAreYouThings(String item, double orderCost, SalesPersonRole salesPerson) {
+		recievedItems = true;
+		bill = orderCost;
+		this.salesPerson = salesPerson;
 	}
 
+	//Scheduler
+	@Override
 	protected boolean pickAndExecuteAnAction() {
+		if (recievedItems == true) {
+			payBill();
+			return true;
+		}
 		return false;
 	}
 
-//	public void setDesire(String string) {
-//		if (string == "buyCar") {
-//			desire = CustomerDesire.buyCar;
-//		}
-//		if (string == "buyFood") {
-//			desire = CustomerDesire.buyCar;
-//		}
-//	}
+	//Actions
+	public void payBill(){
+		salesPerson.msgPayment(this, bill);
+		money -= bill;
+		recievedItems = false;
+	}
+
+	//	public void setDesire(String string) {
+	//		if (string == "buyCar") {
+	//			desire = CustomerDesire.buyCar;
+	//		}
+	//		if (string == "buyFood") {
+	//			desire = CustomerDesire.buyCar;
+	//		}
+	//	}
 }
