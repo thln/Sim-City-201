422a8b479de763c04a1dcaa676c74d1878134504
Kristi Hupka <hupka@usc.edu>
2013-11-15 18:18:53 -0800

    Switched Phonebook package

---
+++ src/application/Phonebook.java
@@ -0,0 +1,17 @@
+package application;
+
+import bank.Bank;
+import market.Market;
+import restaurant.Restaurant;
+
+public class Phonebook {
+	Bank bank;
+	Market market;
+	Restaurant restaurant;
+	
+	//ChineseRestaurant chineseRestaurant;
+	//ItalianRestaurant italianRestaurant;
+	//MexicanRestaurant mexicanRestaurant;
+	//AmericanRestaurant americanRestaurant;
+	//SeafoodRestaurant seafoodRestaurant;
+}
