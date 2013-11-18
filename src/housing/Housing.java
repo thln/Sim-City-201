b0c423cd6bfe594a7f8de6ef2ec147fdd870cf31
unknown <tamnguye@usc.edu>
2013-11-17 20:17:39 -0800

    Added Housing Class

---
+++ src/housing/Housing.java
@@ -0,0 +1,57 @@
+package housing;
+
+import person.Person;
+
+public class Housing 
+{
+	public enum differentHousingTypes {Apartment, Park, Mansion};
+	differentHousingTypes housingStructure;
+	public Person occupant;
+	int housingNumber;
+	int xCordinate;
+	int yCordinate;
+	
+	Housing(Person newP, int Address, String type)
+	{
+		occupant = newP;
+		housingNumber = Address;
+		
+		if(type.equals("Apartment"))
+		{
+			housingStructure = differentHousingTypes.Apartment;
+			//some X and Y coordinate stuff
+		}
+		else if(type.equals("Park"))
+		{
+			housingStructure = differentHousingTypes.Park;
+			//some X and Y Coordinate stuff
+		}
+		else if(type.equals("Mansion"))
+		{
+			housingStructure = differentHousingTypes.Mansion;
+			//some X and Y Coordinate stuff
+		}
+		
+		
+	}
+	
+	public int getHousingNumber()
+	{
+		return housingNumber;
+	}
+	
+	public int getXCordinate()
+	{
+		return xCordinate;
+	}
+	
+	public int getYCordinate()
+	{
+		return yCordinate;
+	}
+	
+	public String getOccupantName()
+	{
+		return "";//occupant.getName();
+	}
+}
