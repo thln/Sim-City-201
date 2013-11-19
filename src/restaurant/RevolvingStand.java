7ad292b6a2c82fdfb4dfa431d59a20deabec6c13
Kristi Hupka <hupka@usc.edu>
2013-11-18 19:02:42 -0800

    Synchronized Revolving stand

--- src/restaurant/RevolvingStand.java
+++ src/restaurant/RevolvingStand.java
@@ -1,26 +1,28 @@
 package restaurant;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class RevolvingStand 
-{
-	List<Order> AllOrders = new ArrayList<Order>();
-	
-	public void newOrder(Order o)
-	{
-		AllOrders.add(o);
+public class RevolvingStand {
+
+	List<Order> AllOrders = Collections.synchronizedList(new ArrayList<Order>());
+
+	public void newOrder(Order o) {
+		synchronized(AllOrders){
+			AllOrders.add(o);
+		}
 	}
-	
-	public Order takeOrder()
-	{
-		Order o = AllOrders.get(0);
-		AllOrders.remove(0);
-		return o;
+
+	public Order takeOrder() {
+		synchronized(AllOrders){
+			Order o = AllOrders.get(0);
+			AllOrders.remove(0);
+			return o;
+		}
 	}
-	
-	public boolean isStandEmpty()
-	{
+
+	public boolean isStandEmpty() {
 		return AllOrders.isEmpty();
 	}
 
