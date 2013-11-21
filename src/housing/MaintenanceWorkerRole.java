055e7ce48add6713c60541f4fb800246b7221387
Kristi Hupka <hupka@usc.edu>
2013-11-20 22:57:00 -0800

    Fixed merge conflicts

--- src/housing/MaintenanceWorkerRole.java
+++ src/housing/MaintenanceWorkerRole.java
@@ -10,102 +10,84 @@ import application.Phonebook;
 import person.Person;
 import person.Role;
 
-public class MaintenanceWorkerRole extends Role 
-{
+public class MaintenanceWorkerRole extends Role {
 	//List of housing
 	//Need to talk more about housing. 
 	//Dynamic number of housing? Cant go through X a day
 	//Or automatically X number of housing every day?
-	
+
 	//public enum housingState {UrgentWorkOrder, CheckUpNeeded, RecentlyChecked};
 	//public housingState state;
 	//state = housingState.CheckUpNeeded;
-	
+
 	//Data
-	
+
 	String name; 
 	private Timer FixingTimer  = new Timer();
 	public enum maintenanceState {Working, CheckingHouse};
 	public maintenanceState state = maintenanceState.Working;
 	//private Semaphore workingOnResidence = new Semaphore(0, true);
-	
-	
-	class WorkOrder 
-	{
+
+
+	class WorkOrder {
 		int HomeNumber;
 		WorkOrder(int n) {
-		    HomeNumber = n;
+			HomeNumber = n;
 		} 
 	};
-		//List  WorkOrders;
-	
-	public MaintenanceWorkerRole(Person person, String pName, String rTitle) 
-	{
+	//List  WorkOrders;
+
+	public MaintenanceWorkerRole (Person person, String pName, String rTitle)  {
 		super(person, pName, rTitle);
 		//this.RoleName = rTitle;
 		//this.name = pName;
 	}
-	
+
 	//Messages
-	public void msgNeedMaintenance(Housing houseNeedMain)
-	{
-		synchronized(Phonebook.publicAllHousing)
-		{
-			for(Housing h : Phonebook.publicAllHousing)
-			{
-				if(h == houseNeedMain)
-				{
+	public void msgNeedMaintenance(Housing houseNeedMain) {
+		synchronized(Phonebook.getPhonebook().publicAllHousing) {
+			for (Housing h : Phonebook.getPhonebook().publicAllHousing) {
+				if (h == houseNeedMain) {
 					h.state = housingState.UrgentWorkOrder;
 				}
 			}
 		}
 	}
-	
+
 	//Scheduler
-	protected boolean pickAndExecuteAnAction() 
-	{
-		if(state == maintenanceState.Working)
-		{
-			synchronized(Phonebook.publicAllHousing)
-			{
-				for(Housing h : Phonebook.publicAllHousing)
-				{
-					if(h.state == housingState.UrgentWorkOrder)
-					{
+	protected boolean pickAndExecuteAnAction() {
+		if (state == maintenanceState.Working) {
+			synchronized(Phonebook.getPhonebook().publicAllHousing) {
+				for (Housing h : Phonebook.getPhonebook().publicAllHousing) {
+					if (h.state == housingState.UrgentWorkOrder) {
 						checkHousing(h);
 						return true;
 					}
 				}
-				
-				for(Housing h : Phonebook.publicAllHousing)
-				{
-					if(h.state == housingState.CheckUpNeeded)
-					{
+
+				for(Housing h : Phonebook.getPhonebook().publicAllHousing) {
+					if (h.state == housingState.CheckUpNeeded) {
 						checkHousing(h);
 						return true;
 					}
-					
+
 				}
 				resetHousingCheck();
 				return true;
-				
 			}
 		}
 		return false;
 	}
-	
+
 	//Actions
-	public void checkHousing(final Housing h)
-	{
+	public void checkHousing(final Housing h) {
 		//For some reason this only runs once
 		state = maintenanceState.CheckingHouse;
 		h.state = housingState.Checking;
 		print("Checking housing " + h.housingNumber + " in state " + h.state);
 		//print("The number of semaphores is: " + workingOnResidence.availablePermits());
-		FixingTimer.schedule(new TimerTask() 
-		{
-			public void run() 
-			{
+		FixingTimer.schedule(new TimerTask() {
+			public void run() {
 				//o.state = FoodState.Ready;
 				state = maintenanceState.Working;
 				h.state = housingState.RecentlyChecked;
@@ -118,13 +100,10 @@ public class MaintenanceWorkerRole extends Role
 		},
 		2000);
 	}
-	
-	public void resetHousingCheck()
-	{
-		synchronized(Phonebook.publicAllHousing)
-		{
-			for(Housing h : Phonebook.publicAllHousing)
-			{
+
+	public void resetHousingCheck() {
+		synchronized(Phonebook.getPhonebook().publicAllHousing) {
+			for (Housing h : Phonebook.getPhonebook().publicAllHousing) {
 				print("All houses are finished! Rechecking houses");
 				h.state = housingState.CheckUpNeeded;
 			}
