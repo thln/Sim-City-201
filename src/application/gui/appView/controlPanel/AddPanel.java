cfe3a34119a6d7efd216fc5fac3eaa68aec68001
Kristi Hupka <hupka@usc.edu>
2013-11-22 15:47:04 -0800

    Removed last name field

--- src/application/gui/appView/controlPanel/AddPanel.java
+++ src/application/gui/appView/controlPanel/AddPanel.java
@@ -14,8 +14,6 @@ public class AddPanel implements ActionListener {
 	public static JPanel mainPanel = new JPanel();
 	private JLabel first = new JLabel("First Name: ");
 	private JTextField firstName = new JTextField(10);
-	private JLabel last = new JLabel("Last Name: ");
-	private JTextField lastName = new JTextField(10);
 	private JButton addButton = new JButton("Add");
 	private ControlPanel cp;
 	private Application app;
@@ -38,14 +36,6 @@ public class AddPanel implements ActionListener {
 		c.gridy = 1;
 		mainPanel.add(firstName);
 		
-		c.gridx = 1;
-		c.gridy = 0;
-		mainPanel.add(last,c);
-		
-		c.gridx = 1;
-		c.gridy = 1;
-		mainPanel.add(lastName,c);
-		
 		c.gridx = 0;
 		c.gridy	= 2;
 		addButton.addActionListener(this);
@@ -66,7 +56,7 @@ public class AddPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == addButton){
-				String name = firstName.getText() +" "  + lastName.getText();
+				String name = firstName.getText();
 				String type = (String)typeBox.getSelectedItem();
 				app.addPerson(name, 500, type, "location", null, 0, 0, 0);
 				app.printLastPop();
