b9882eb26a147acb2007f5f1902c3d7f01e374e9
unknown <tamnguye@usc.edu>
2013-11-25 01:05:25 -0800

    Made mailbox file

---
+++ src/housing/Mailbox.java
@@ -0,0 +1,11 @@
+package housing;
+
+/**** This class is purely for 
+ * the Wealthy people aka Landlords to pick up money and for everyone else in the city
+ * to drop off money. Additionally, since everyone outside of wealthy live in apartments or 
+ * parks, and people who live in the park don't pay, all the payments are the same ***/
+
+public class Mailbox 
+{
+
+}
