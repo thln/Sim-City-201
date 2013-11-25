f537fc15019340e5730947b17769535083231a55
Josh Greenberger <jdgreenb@usc.edu>
2013-11-24 17:00:22 -0800

    Fixing restaurant assignment errors

--- src/person/Worker.java
+++ src/person/Worker.java
@@ -180,6 +180,7 @@ public class Worker extends Person {
         //Actions
         public void prepareForWork() {
 
+        	print("Preparing for work as " + myJob.title);
                 if (myJob.jobPlace == "bank") 
                 {
                         workerRole = Phonebook.getPhonebook().getBank().arrivedAtWork(this, myJob.title);
