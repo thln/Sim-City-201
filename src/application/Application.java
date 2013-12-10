8f4800ad6f6a1a79cf8dadc1131264fd7b84b591
Kristi Hupka <hupka@usc.edu>
2013-12-09 19:25:20 -0800

    Chinese restaurant works with multiple markets

--- src/application/Application.java
+++ src/application/Application.java
@@ -28,9 +28,6 @@ public class Application extends JPanel {
 	private static List<Housing> allHousing = Collections.synchronizedList(new ArrayList<Housing>());
 
 	public AnimationPanel animPanel;
-	public Bank bank;
-	public Market market;
-	public ChineseRestaurant chineseRestaurant;
 	public Timer updateTimer = new Timer();
 
 	//public static Phonebook phonebook = new Phonebook(bank, market, restaurant, allHousing);
@@ -39,8 +36,10 @@ public class Application extends JPanel {
 	public Application(AnimationPanel ap) {
 
 		animPanel = ap;
-		//the following line is for dynamic building and business making in v2
-		//Phonebook.getPhonebook().getBusinessFromGui(animPanel);
+		//Adding markets to the cook in Chinese Restaurant
+		Phonebook.getPhonebook().getChineseRestaurant().chineseRestaurantCookRole.addMarket(Phonebook.getPhonebook().getEastMarket());
+		Phonebook.getPhonebook().getChineseRestaurant().chineseRestaurantCookRole.addMarket(Phonebook.getPhonebook().getWestMarket());
+	
 
 		//String name, int money, String jobTitle, String jobPlace, int startT, int lunchT, int endT
 
