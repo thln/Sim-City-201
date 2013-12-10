4c473bda0b691cb3dfc5786566aeb5eec5aede7e
Kristi Hupka <hupka@usc.edu>
2013-12-09 17:15:16 -0800

    Fixing Chinese restaurant

--- src/chineseRestaurant/ChineseRestaurantCookRole.java
+++ src/chineseRestaurant/ChineseRestaurantCookRole.java
@@ -55,7 +55,7 @@ public class ChineseRestaurantCookRole extends Role implements ChineseRestaurant
 		super(p1, pName, rName);
 		chineseRestaurant = restaurant;
 		//Starts revolving stand timer to check revolving stand
-		startRevolvingStandTimer();
+		//startRevolvingStandTimer();
 		//theRevolvingStand = Phonebook.getPhonebook().getRestaurant().getRevolvingStand();
 
 	}
@@ -63,7 +63,7 @@ public class ChineseRestaurantCookRole extends Role implements ChineseRestaurant
 	public ChineseRestaurantCookRole(String roleName, ChineseRestaurant chineseRestaurant) {
 		super(roleName);
 		//Starts revolving stand timer to check revolving stand
-		startRevolvingStandTimer();
+		//startRevolvingStandTimer();
 		//this.restaurant = restaurant;
 		//theRevolvingStand = Phonebook.getPhonebook().getRestaurant().getRevolvingStand();
 	}
